25afd0ac491fba649bf726fa6ff95e98a4881907
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-07-02 00:26:08 +0100

    Update Employee class with Project and Department

--- src/main/java/com/example/codeclan/employees/models/Employee.java
+++ src/main/java/com/example/codeclan/employees/models/Employee.java
@@ -1,14 +1,16 @@
 package com.example.codeclan.employees.models;
 
-import org.springframework.web.bind.annotation.PathVariable;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "employees")
 public class Employee {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "id", nullable = false)
     private Long id;
 
@@ -24,11 +26,39 @@ public class Employee {
     @Column(name = "email", nullable = false)
     private String email;
 
-    public Employee(String name, int age, int employeeNumber, String email) {
+    @ManyToOne
+    @JsonIgnoreProperties(value = "employees")
+    @JoinColumn(name = "department_id", nullable = false)
+    private Department department;
+
+    @ManyToMany
+    @JsonIgnoreProperties(value = "employees")
+    @JoinTable(
+            name = "projects_employees",
+            joinColumns = {
+                    @JoinColumn(
+                            name = "employee_id",
+                            nullable = false,
+                            updatable = false
+                    )
+            },
+            inverseJoinColumns = {
+                    @JoinColumn(
+                            name = "project_id",
+                            nullable = false,
+                            updatable = false
+                    )
+            }
+    )
+    private List<Project> projects;
+
+    public Employee(String name, int age, int employeeNumber, String email, Department department) {
         this.name = name;
         this.age = age;
         this.employeeNumber = employeeNumber;
         this.email = email;
+        this.department = department;
+        this.projects = new ArrayList<>();
     }
 
     public Employee() {
@@ -74,5 +104,26 @@ public class Employee {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public List<Project> getProjects() {
+        return projects;
+    }
+
+    public void setProjects(List<Project> projects) {
+        this.projects = projects;
+    }
+
+    public void addProject(Project project) {
+        getProjects().add(project);
+    }
+
     //</editor-fold>
 }
